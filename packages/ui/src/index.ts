1e3a3bd68200ac2eb6a996c709d58e1b7f6ba438
scerelli <hi@stefanocerelli.com>
2026-01-23 14:55:43 +0100

    feat(ui): add components library

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,14 @@
+// Components
+export * from "./components/button";
+export * from "./components/input";
+export * from "./components/separator";
+export * from "./components/sheet";
+export * from "./components/sidebar";
+export * from "./components/skeleton";
+export * from "./components/tooltip";
+
+// Utilities
+export { cn } from "./lib/utils";
+
+// Hooks
+export { useIsMobile } from "./hooks/use-mobile";
