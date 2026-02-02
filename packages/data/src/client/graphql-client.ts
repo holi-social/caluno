55c82a993556d85504fc85108410a9b8e7d453b7
scerelli <hi@stefanocerelli.com>
2026-02-02 01:29:56 +0100

    feat(web): inject headers in graphql client

--- packages/data/src/client/graphql-client.ts
+++ packages/data/src/client/graphql-client.ts
@@ -3,14 +3,29 @@ import { GraphQLClient } from 'graphql-request';
 export interface GraphQLClientConfig {
   url: string;
   credentials?: RequestCredentials;
-  headers?: Record<string, string>;
+  headers?: Record<string, string> | (() => Record<string, string>);
 }
 
 export function createGraphQLClient(
   config: GraphQLClientConfig,
 ): GraphQLClient {
-  return new GraphQLClient(config.url, {
+  const client = new GraphQLClient(config.url, {
     credentials: config.credentials,
-    headers: config.headers,
+    headers: typeof config.headers === 'function' ? undefined : config.headers,
   });
+
+  if (typeof config.headers === 'function') {
+    const getHeaders = config.headers;
+    client.requestConfig.requestMiddleware = (request) => {
+      return {
+        ...request,
+        headers: {
+          ...request.headers,
+          ...getHeaders(),
+        },
+      };
+    };
+  }
+
+  return client;
 }
