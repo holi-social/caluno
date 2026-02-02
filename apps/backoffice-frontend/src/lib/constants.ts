92635e956ae273ff7a485946ff343af1a913a07c
scerelli <hi@stefanocerelli.com>
2026-02-02 01:33:05 +0100

    feat(web): consts

---
+++ apps/backoffice-frontend/src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const ORG_CONTEXT_COOKIE = 'clippy.current_org';
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001';
+export const GRAPHQL_API_URL = `${API_URL}/graphql`;
