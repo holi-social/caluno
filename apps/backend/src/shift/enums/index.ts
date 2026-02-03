fb0d5b6989ea20b30abcd7684b15e5acd0ee8089
Taha Cherfia <taha.cherfia@gmail.com>
2026-02-03 11:20:15 +0100

    feat(be): add shift invite status enum

--- apps/backend/src/shift/enums/index.ts
+++ apps/backend/src/shift/enums/index.ts
@@ -2,3 +2,9 @@ export enum ShiftVisibility {
   INVITED_MEMBERS = 'INVITED_MEMBERS',
   ALL_MEMBERS = 'ALL_MEMBERS',
 }
+
+export enum ShiftInviteStatus {
+  PENDING = 'PENDING',
+  ACCEPTED = 'ACCEPTED',
+  REJECTED = 'REJECTED',
+}
