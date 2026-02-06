5827838f59f0d42bbcf3ed87232a41dbb69bd9ea
Dima Rosmait <dima.rosmait@gmail.social>
2026-02-06 10:20:43 +0100

    feat(data): add queries for fetching single and multiple projects in ProjectRepository

--- packages/data/src/repositories/project/project.repository.ts
+++ packages/data/src/repositories/project/project.repository.ts
@@ -8,6 +8,31 @@ export interface FindProjectsOptions {
 }
 
 export class ProjectRepository extends BaseRepository {
+  async findById(id: string) {
+    try {
+      const data = await this.sdk.GetProject({ id });
+      return data.project;
+    } catch (error) {
+      throw DataError.fromGraphQLError(error);
+    }
+  }
+
+  async findAllByOrganizationId(
+    organizationId: string,
+    options: FindProjectsOptions = {},
+  ) {
+    try {
+      const data = await this.sdk.GetProjects({
+        organizationId,
+        limit: options.limit ?? 10,
+        offset: options.offset ?? 0,
+      });
+      return data.projects;
+    } catch (error) {
+      throw DataError.fromGraphQLError(error);
+    }
+  }
+
   async create(input: CreateProjectInput) {
     try {
       const data = await this.sdk.CreateProject({ input });
