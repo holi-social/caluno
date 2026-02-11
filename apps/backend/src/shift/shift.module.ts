e95ee44328ac5ba5d35e8ef243e2e947d1866b55
Tom Philip <tom@tomphilip.me>
2026-02-11 14:41:28 +0100

    fix: was missing its module import

--- apps/backend/src/shift/shift.module.ts
+++ apps/backend/src/shift/shift.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MembershipModule } from 'src/membership/membership.module';
 import { UserModule } from 'src/user/user.module';
 import { DatabaseModule } from '../database/database.module';
+import { MembershipModule } from '../membership/membership.module';
 import { ShiftMapper } from './mappers/shift.mapper';
 import { ShiftFieldResolver } from './resolvers/shift-field.resolver';
 import { ShiftMutationResolver } from './resolvers/shift-mutation.resolver';
