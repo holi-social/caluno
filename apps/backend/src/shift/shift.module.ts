fc2d493f9cd875e401a2e2def308a4334183d978
Dima Rosmait <dima.rosmait@gmail.social>
2026-02-12 12:45:29 +0100

    feat(be): add MembershipModule to ShiftModule imports

--- apps/backend/src/shift/shift.module.ts
+++ apps/backend/src/shift/shift.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from 'src/user/user.module';
 import { DatabaseModule } from '../database/database.module';
+import { MembershipModule } from '../membership/membership.module';
 import { ShiftMapper } from './mappers/shift.mapper';
 import { ShiftFieldResolver } from './resolvers/shift-field.resolver';
 import { ShiftMutationResolver } from './resolvers/shift-mutation.resolver';
@@ -8,7 +9,7 @@ import { ShiftQueryResolver } from './resolvers/shift-query.resolver';
 import { ShiftService } from './shift.service';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [DatabaseModule, UserModule, MembershipModule],
   providers: [
     ShiftService,
     ShiftQueryResolver,
