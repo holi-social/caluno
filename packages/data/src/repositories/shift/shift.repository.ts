0b69f5b1f7b08f2beecbed2c45877fab3fb4bb9a
Dima Rosmait <dima.rosmait@gmail.social>
2026-02-16 15:06:05 +0100

    feat(data): add GetShiftsByProjectId query and implement findAllByProjectId method in ShiftRepository

--- packages/data/src/repositories/shift/shift.repository.ts
+++ packages/data/src/repositories/shift/shift.repository.ts
@@ -2,11 +2,32 @@ import { DataError } from '../../errors/data-error';
 import type { CreateShiftInput } from '../../generated/graphql';
 import { BaseRepository } from '../base/base.repository';
 
+export interface PagginationOptions {
+  limit?: number;
+  offset?: number;
+}
+
 export class ShiftRepository extends BaseRepository {
   async create(input: CreateShiftInput) {
     try {
       const data = await this.sdk.CreateShift({ input });
       return data.createShift;
+    } catch (error) {
+      throw DataError.fromGraphQLError(error);
+    }
+  }
+
+  async findAllByProjectId(
+    projectId: string,
+    options: PagginationOptions = {},
+  ) {
+    try {
+      const data = await this.sdk.GetShiftsByProjectId({
+        projectId,
+        limit: options.limit ?? 10,
+        offset: options.offset ?? 0,
+      });
+      return data.shiftsByProjectId;
     } catch (error) {
       throw DataError.fromGraphQLError(error);
     }
