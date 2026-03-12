6c13f906b9df6d845629dc30fb1c2606297b944e
Tom Philip <tom@tomphilip.me>
2026-03-12 18:21:31 +0100

    feat: basic thinking for check-in status/validity

--- apps/backoffice-frontend/src/app/(dashboard)/[orgId]/check-in/[checkInId]/page.tsx
+++ apps/backoffice-frontend/src/app/(dashboard)/[orgId]/check-in/[checkInId]/page.tsx
@@ -8,7 +8,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@repo/ui';
-import { AlertCircle, User } from 'lucide-react';
+import { AlertCircle, CheckCircle2, User } from 'lucide-react';
 import Image from 'next/image';
 import { getDataClient } from '@/lib/data-client';
 
@@ -16,11 +16,17 @@ interface CheckinPageProps {
   params: Promise<{ orgId: string; checkInId: string }>;
 }
 
+type CheckInStatus =
+  | 'valid'
+  | 'blocked'
+  | 'information-required'
+  | 'id-check'
+  | 'already-checked-in';
+
 export default async function CheckinPage({ params }: CheckinPageProps) {
   const { orgId, checkInId } = await params;
 
   const data = await getDataClient();
-
   const user = await data.user.findByCheckInId(checkInId);
 
   if (!user) {
@@ -37,36 +43,51 @@ export default async function CheckinPage({ params }: CheckinPageProps) {
     );
   }
 
+  const status: CheckInStatus = 'valid';
+
   return (
     <div className="max-w-2xl">
       <div>
         <div>
           <h1 className="text-3xl font-bold">Check-in</h1>
-          <p className="text-muted-foreground">
-            Check the details and check them into a shift
-          </p>
         </div>
-        <div className="px-2 py-8 space-y-4">
-          <Card>
-            <CardHeader>
+        <div className="lg:px-2 lg:py-8 py-4 space-y-4">
+          {status === 'blocked' && (
+            <Alert variant="destructive">
+              <AlertCircle />
+              <AlertTitle>Check-in denied</AlertTitle>
+              <AlertDescription>
+                The volunteer is not permitted to check-in. They must contact
+                Headquarters for assistance.
+              </AlertDescription>
+            </Alert>
+          )}
+
+          <Card className="p-4 gap-2">
+            <CardHeader className="px-0">
               <CardTitle>
                 {user.name} <span className="font-light">({user.email})</span>
               </CardTitle>
             </CardHeader>
-            <CardContent>
+            <CardContent className="px-0">
               {user.image ? (
                 <Image src={user.image} alt="Volunteers profile photo" />
               ) : (
                 <div className="border-8 rounded-2xl inline-block">
-                  <User className="size-72 text-accent/90" />
+                  <User className="size-72 max-w-full text-accent/90" />
                 </div>
               )}
             </CardContent>
           </Card>
           <div className="grid grid-cols-2 gap-4">
             <Card>
-              <CardContent>
+              <CardHeader>
                 <CardTitle>Requirements</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="flex gap-2">
+                  <CheckCircle2 className="text-green-500" /> All met
+                </div>
               </CardContent>
             </Card>
             <Card>
@@ -76,7 +97,7 @@ export default async function CheckinPage({ params }: CheckinPageProps) {
             </Card>
           </div>
 
-          <Button size="lg" className="w-full">
+          <Button size="lg" className="w-full" disabled={status !== 'valid'}>
             Check-in
           </Button>
         </div>
