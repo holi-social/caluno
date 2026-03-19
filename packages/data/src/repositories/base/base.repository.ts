c00ae8622f364e9905d6f06020fe3242ed82348b
Tom Philip <tom@tomphilip.me>
2026-03-19 13:27:15 +0100

    fix: Fix up the Fe, so it now works with just Time Entry and no longer with the deleted Task & Volunteer Session

--- packages/data/src/repositories/base/base.repository.ts
+++ packages/data/src/repositories/base/base.repository.ts
@@ -1,6 +1,11 @@
 import type { GraphQLClient } from 'graphql-request';
 import { getSdk } from '../../generated/graphql';
 
+export interface PaginationOptions {
+  limit?: number;
+  offset?: number;
+}
+
 export abstract class BaseRepository {
   protected readonly sdk: ReturnType<typeof getSdk>;
 
