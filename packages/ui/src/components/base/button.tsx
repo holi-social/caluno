035f5506390aa3e7a8f1859affd1d444b82ef9ad
Tom Philip <tom@tomphilip.me>
2026-04-01 13:43:06 +0200

    feat: bumping up the size of button and defaulting to large size, if non specified

--- packages/ui/src/components/base/button.tsx
+++ packages/ui/src/components/base/button.tsx
@@ -5,7 +5,7 @@ import type * as React from 'react';
 import { cn } from '../../lib/utils';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xl text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xl text-base font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-5 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
@@ -21,19 +21,19 @@ const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-9 px-4 py-2 has-[>svg]:px-3',
         xs: "h-6 gap-1 rounded-xl px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
-        sm: 'h-8 rounded-xl gap-1.5 px-3 has-[>svg]:px-2.5',
-        lg: 'h-10 rounded-xl px-6 has-[>svg]:px-4',
-        icon: 'size-9',
+        sm: "h-8 rounded-xl gap-1.5 px-3 has-[>svg]:px-2.5 [&_svg:not([class*='size-'])]:size-4",
+        base: 'h-9 px-4 py-2 has-[>svg]:px-3',
+        lg: 'h-10 rounded-xl py-2 px-9 has-[>svg]:px-7',
+        icon: 'size-10',
         'icon-xs': "size-6 rounded-xl [&_svg:not([class*='size-'])]:size-3",
-        'icon-sm': 'size-8',
-        'icon-lg': 'size-10',
+        'icon-sm': "size-8 [&_svg:not([class*='size-'])]:size-4",
+        'icon-base': 'size-9',
       },
     },
     defaultVariants: {
       variant: 'default',
-      size: 'default',
+      size: 'lg',
     },
   },
 );
@@ -41,7 +41,7 @@ const buttonVariants = cva(
 function Button({
   className,
   variant = 'default',
-  size = 'default',
+  size = 'lg',
   asChild = false,
   ...props
 }: React.ComponentProps<'button'> &
