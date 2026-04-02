8a8093254f5aa5260cc4d67a406bdd7ffcada65c
scerelli <hi@stefanocerelli.com>
2026-04-02 18:50:34 +0200

    refactor(frontend): increase create shift button size

--- apps/frontend/src/components/create-shift-button.tsx
+++ apps/frontend/src/components/create-shift-button.tsx
@@ -7,7 +7,7 @@ import { useSheet } from '@/hooks/use-sheet';
 export function CreateShiftButton() {
   const { open } = useSheet('shift', 'id');
   return (
-    <Button onClick={() => open()}>
+    <Button size="lg" onClick={() => open()}>
       <PlusIcon className="mr-2 h-4 w-4" /> Create Shift
     </Button>
   );
