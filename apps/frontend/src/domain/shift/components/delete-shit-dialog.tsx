2bc2df231d2a5b729f9523b85dd8fae34d90068e
Tom Philip <tom@tomphilip.me>
2026-04-07 18:15:02 +0200

    feat: Alert Dialogs over simple confirm()

---
+++ apps/frontend/src/domain/shift/components/delete-shit-dialog.tsx
@@ -0,0 +1,47 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@repo/ui';
+
+interface DeleteShiftDialogProps {
+  onDelete: () => void;
+  trigger: React.ReactNode;
+}
+
+export function DeleteShiftDialog({
+  onDelete,
+  trigger,
+}: DeleteShiftDialogProps) {
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        {trigger}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Delete shift</AlertDialogTitle>
+          <AlertDialogDescription>
+            Are you sure you wish to delete this shift and all it's timesheets?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={onDelete}
+            variant="destructive"
+          >
+            Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
