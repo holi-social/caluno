8c1a967e09473c0837e4ddadc48d32fe453b42e2
Taha Cherfia <taha.cherfia@gmail.com>
2026-05-11 15:12:46 +0200

    feat(backend): add organization tree model

---
+++ apps/backend/src/organization/models/organization-tree.model.ts
@@ -0,0 +1,15 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { GraphQLJSON } from 'graphql-scalars';
+import type { OrganizationNode } from '../types/organization-node';
+
+@ObjectType()
+export class OrganizationTree {
+  @Field(() => ID)
+  id: string;
+
+  @Field(() => String)
+  name: string;
+
+  @Field(() => GraphQLJSON)
+  nodes: OrganizationNode[];
+}
