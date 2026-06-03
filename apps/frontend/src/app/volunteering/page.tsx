b8a6f50705b5f9003384d66959e0a140eb568c21
Tom Philip <tom@tomphilip.me>
2026-06-03 18:35:55 +0200

    feat: volunteer sit shell

---
+++ apps/frontend/src/app/volunteering/page.tsx
@@ -0,0 +1,7 @@
+export default async function OrgUnitHomePage() {
+  return (
+    <div>
+      <h1 className="page-title">Home</h1>
+    </div>
+  );
+}
---
+++ apps/frontend/src/app/volunteering/profile/page.tsx
@@ -0,0 +1,7 @@
+export default async function ProfilePage() {
+  return (
+    <div>
+      <h1 className="page-title">Edit Profile</h1>
+    </div>
+  );
+}
