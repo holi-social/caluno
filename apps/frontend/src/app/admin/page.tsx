c8997282142509db4a4c990234838e8847fdd1ee
Tom Philip <tom@tomphilip.me>
2026-06-04 16:05:16 +0200

    refactor: check is in the layout

--- apps/frontend/src/app/admin/page.tsx
+++ apps/frontend/src/app/admin/page.tsx
@@ -1,16 +1,11 @@
 import { redirect } from 'next/navigation';
 import HomePage from '@/domain/home/components/home-page';
-import { isAuthenticated } from '@/lib/auth-server';
 import {
   getLastVisitedOrgServer,
   getMyAccessibleOrganizationUnits,
 } from '@/lib/org-context-server';
 
 export default async function Home() {
-  if (!(await isAuthenticated())) {
-    return redirect('/login');
-  }
-
   const orgs = await getMyAccessibleOrganizationUnits();
 
   const lastorgUId = await getLastVisitedOrgServer();
