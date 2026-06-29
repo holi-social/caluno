ca88dfecb3738aad96b668a0696108e6cfbeb840
scerelli <hi@stefanocerelli.com>
2026-06-29 18:18:25 +0200

    feat(auth): add updateMyLocale mutation and user locale resolution

--- apps/backend/src/user/user.module.ts
+++ apps/backend/src/user/user.module.ts
@@ -3,12 +3,19 @@ import { AuthModule } from '../auth/auth.module';
 import { DatabaseModule } from '../database/database.module';
 import { UserMapper } from './mappers/user.mapper';
 import { UserFieldResolver } from './resolvers/user-field.resolver';
+import { UserMutationResolver } from './resolvers/user-mutation.resolver';
 import { UserQueryResolver } from './resolvers/user-query.resolver';
 import { UserService } from './user.service';
 
 @Module({
   imports: [DatabaseModule, AuthModule],
-  providers: [UserService, UserQueryResolver, UserFieldResolver, UserMapper],
+  providers: [
+    UserService,
+    UserQueryResolver,
+    UserMutationResolver,
+    UserFieldResolver,
+    UserMapper,
+  ],
   exports: [UserService, UserMapper],
 })
 export class UserModule {}
