e74b6a7549fca6081c0e1752b2b2332e704e6de7
Tom Philip <tom@tomphilip.me>
2026-07-07 17:09:50 +0200

    feat: simplify naming

--- apps/frontend/src/components/navigation/dashboard-sidebar.tsx
+++ apps/frontend/src/components/navigation/dashboard-sidebar.tsx
@@ -75,7 +75,7 @@ export function DashboardSidebar({ permissions }: DashboardSidebarProps) {
         count: pendingCount,
       },
       {
-        titleKey: 'checkInOut',
+        titleKey: 'checkIn',
         href: `/admin/${orgUId}/check-in/scan`,
         icon: ScanQrCode,
       },
