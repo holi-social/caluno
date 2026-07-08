01087c19f06aafe5a57d14cd2b65365198e9374d
Anastasiia Kryvoshei <anastasiia.kryvoshei@holi.social>
2026-07-08 16:03:49 +0200

    fix: cleanup of extra stuff for dev build without auth

--- apps/frontend/src/app/[locale]/dev/accounting-templates/page.tsx
+++
@@ -1,5 +0,0 @@
-import { DevTemplateListingPreview } from '@/domain/accounting/components/template/listing-dev-preview';
-
-export default function DevAccountingTemplatesPage() {
-  return <DevTemplateListingPreview />;
-}
