82a66ff3075f49cac3319a04680f7274083f7aa2
Malte Finsterwalder <malte.finsterwalder@holi.social>
2026-07-14 16:13:09 +0200

    fix(be): remove default permission from Member role

--- apps/backend/src/auth/constants/index.ts
+++ apps/backend/src/auth/constants/index.ts
@@ -3,7 +3,7 @@ import { PermissionKey } from '../enums';
 export { PERMISSION_GROUPS } from './permission-groups';
 export const PERMISSIONS = PermissionKey;
 
-export const MEMBER_DEFAULT_PERMISSIONS = [PERMISSIONS.ORG_VIEW];
+export const MEMBER_DEFAULT_PERMISSIONS = [];
 
 export const DEFAULT_OWNER_ROLE_NAME = 'Owner';
 export const DEFAULT_MEMBER_ROLE_NAME = 'Member';
