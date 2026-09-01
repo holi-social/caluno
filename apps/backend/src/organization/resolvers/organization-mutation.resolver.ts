a8fb6079dc87a4e0babf59249e07594a8a781ce2
scerelli <hi@stefanocerelli.com>
2026-09-01 16:43:51 +0200

    fix(organization): require ORG_EDIT permission on updateOrganization mutation

--- apps/backend/src/organization/resolvers/organization-mutation.resolver.ts
+++ apps/backend/src/organization/resolvers/organization-mutation.resolver.ts
@@ -1,6 +1,8 @@
 import { Args, ID, Mutation, Resolver } from '@nestjs/graphql';
 import type { UserSession } from '@thallesp/nestjs-better-auth';
 import { Session } from '@thallesp/nestjs-better-auth';
+import { PERMISSIONS } from '../../auth/constants';
+import { Permissions } from '../../auth/decorators/permissions.decorator';
 import { CreateOrganizationInput } from '../inputs/create-organization.input';
 import { UpdateOrganizationInput } from '../inputs/update-organization.input';
 import { Organization } from '../models/organization.model';
@@ -18,6 +20,7 @@ export class OrganizationMutationResolver {
     return this.organizationService.create(session.user.id, input);
   }
 
+  @Permissions(PERMISSIONS.ORG_EDIT)
   @Mutation(() => Organization)
   async updateOrganization(
     @Args('id', { type: () => ID }) id: string,
